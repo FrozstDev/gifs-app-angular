18e7034342cc1b0dcb67192cf1f7d5f310468bef
Allen Vargas <frozstdev@gmail.com>
2025-08-22 01:50:33 -0400

    Show gifs by trending, search ang history using localStorage

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,9 @@ export const environment = {
   production: true,
   companyName: 'Gifs',
   companyName2: 'App',
-  companySlogan: 'Maneja tus gifs'
+  companySlogan: 'Maneja tus gifs',
 
+  // ApiKeys
+  apiKey: 'FGSbCuT5EDDWHlN9NNAuge3PhBWKs75v',
+  giphyUrl: 'https://api.giphy.com/v1',
 };
